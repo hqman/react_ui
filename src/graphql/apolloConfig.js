755d8b1e6f00f990e6165547f8c6d61c9a1cd2bd
hqman <hqmank@gmail.com>
2019-03-14 11:04:38 +0800

    update apollo client config

--- src/graphql/apolloConfig.js
+++ src/graphql/apolloConfig.js
@@ -1,48 +1,26 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from "apollo-client";
-import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
-
 import { resolvers, typeDefs } from './resolvers';
 
 
-const AUTH_TOKEN = 'token'
-//从local取 当前管理公司信息
-export const get_current_company = () => {
-  const company_object = localStorage.getItem('current_company')
-  if (company_object) {
-    return JSON.parse(company_object)
-  }
-  return null
-}
-let currentCompanyId = get_current_company() ? get_current_company().id : null
-const httpLink = new HttpLink({ uri: 'http://localhost:5000/graphql' })
-const middlewareLink = new ApolloLink((operation, forward) => {
-  // get the authentication token from local storage if it exists
-  const tokenValue = localStorage.getItem(AUTH_TOKEN)
-  // debugger
-  if (tokenValue) {
-    // return the headers to the context so httpLink can read them
-    operation.setContext({
-      headers: {
-        Authorizationjwt_required: `Bearer ${tokenValue}`,
-        company_id: currentCompanyId ? currentCompanyId : 0,
-      },
-    })
-  }
-
-
-  return forward(operation)
-})
-
-// authenticated httplink
-const httpLinkAuth = middlewareLink.concat(httpLink)
-
 const cache = new InMemoryCache();
+const tokenValue = localStorage.getItem('token')
+const currentCompanyId = localStorage.getItem('current_company_id')
+// console.log(currentCompanyId ? currentCompanyId : 0);
 
 const client = new ApolloClient({
   cache,
-  httpLinkAuth,
+  // httpLinkAuth,
+  link: new HttpLink({
+    uri: 'http://localhost:5000/graphql',
+    headers: {
+      Authorization: `Bearer ${tokenValue}`,
+      company_id: currentCompanyId ? currentCompanyId : 0,
+      'client-name': 'smq[pc_client]',
+      'client-version': '1.0.0',
+    },
+  }),
   resolvers,
   typeDefs,
 });
